0ad96ef0b0549da9ce984ad8e6bd36a509cb1638
alex.lopez <alex.lopez@personio.de>
2021-01-06 20:25:44 +0100

    Add example app

---
+++ ExampleApp/index.php
@@ -0,0 +1,7 @@
+<?php
+
+echo "Hello world! v3";
+echo "ENV_VAR_1 = ".getenv("ENV_VAR_1")."\n";
+echo "ENV_VAR_2 = ".getenv("ENV_VAR_2")."\n";
+echo "DB_USER = ".getenv("DB_USER")."\n";
+echo "DB_PASS = ".getenv("DB_PASS")."\n";
